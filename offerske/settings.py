9c4f2a494b406410093a83615fb313eed8649208
Dennis moseti <dudubot404@gmail.com>
2024-03-23 11:35:41 -0400

    pipeline: added the mongo pipeline for item export with the setting needed to run it

--- offerske/settings.py
+++ offerske/settings.py
@@ -26,13 +26,16 @@ CONCURRENT_REQUESTS = 1
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
+
+
+MONGO_URI = ''
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -63,9 +66,10 @@ CONCURRENT_REQUESTS = 1
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "offerske.pipelines.OfferskePipeline": 300,
-#}
+ITEM_PIPELINES = {
+    # "offerske.pipelines.OfferskePipeline": 300,
+    "offerske.pipelines.MongoDBPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
